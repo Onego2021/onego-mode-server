5641fc91d4c21402481f9d160df83f1bd9055ff7
judyi19997 <judyi1999@kookmin.ac.kr>
2021-09-08 03:00:14 +0900

    join router

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ s3 = boto3.client('s3',
     aws_access_key_id = AWS_ACCESS_KEY,
     aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
 )
+model_base_url = 'http://3.38.9.213:5000'
+main_base_url = 'http://3.34.215.157:3000'
 
 @app.route('/', methods=["GET"])
 def hello():
@@ -19,55 +21,33 @@ def hello():
 def onego_hello():
     return "Welcome to ONEGO!"
 
-@app.route("/onego_recognize", methods=['POST','GET'])
-def onego_recognize():
-    file = ""
-    filename = ""
-    path = "input/manuscript_input" + filename
-    uid = request.args["uid"]
-
-    if request.method == 'POST':
-        file = request.file['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(path))
-
-    elif request.method == 'GET':
-        pred_list = ["원","고","지", " ", "인", "식", "이", " ", "돼", "어", "잇", "는", "파", "일", "입", "니", "다", "."]
-        pred_str = ''.join(pred_list)
-        os.makedirs('./tmp', exist_ok=True)
-        f = open('./tmp/{}.txt'.format(uid),'w',encoding='UTF-8')
-        print('Complete create file')
-        f.write(pred_str)
-        f.close()
-        try:
-            txt_path = './tmp/{}.txt'.format(uid)
-            s3.upload_file(txt_path, AWS_S3_BUCKET_NAME, '{}/after_onego.txt'.format(uid))
-            print("complete upload .txt to s3")
-        except Exception as e:
-            print(e)
-            return "False..."
-    
-# @app.route("/test", methods=["GET"])
-# def testPostAPI():
-#     temp = request.args["uid"]
-#     data = "test data"
-#     return jsonify({
-#         'result': data,
-#         'uid' : temp,
-#     })
-
-@app.route("/file_download", methods = ["GET"])
+@app.route("/file_download", methods=['POST','GET'])
 def file_download():
+    pred_list = ["원","고","지", " ", "인", "식", "이", " ", "돼", "어", "잇", "는", "파", "일", "입", "니", "다", "."]
     uid = request.args["uid"]
     file_path = './input/manuscript_input/{}.png'.format(uid)
-    print(uid)
     try:
         s3.download_file(AWS_S3_BUCKET_NAME,"{}/Pororo.png".format(uid),file_path)
         print("complete download .png from s3!")
-        return redirect(url_for('onego_recognize',uid = uid))
+        # pred_list = demo() --detect함수 실행하는 부분
+
+        os.makedirs('./tmp', exist_ok=True)
+        f = open('./tmp/{}.txt'.format(uid),'w',encoding='UTF-8')
+        pred_str = ''.join(pred_list)
+        f.write(pred_str)
+        f.close()
+        print('Complete create file')
+        try:
+            txt_path = './tmp/{}.txt'.format(uid)
+            s3.upload_file(txt_path, AWS_S3_BUCKET_NAME, '{}/before_onego.txt'.format(uid))
+            return("complete upload .txt to s3")
+        except Exception as e:
+            print(e)
+            return "false upload .txt to s3"
     except Exception as e:
         print(e)
-        return "flase..."
+        return "flase download .png from s3..."
+
 
 if __name__ == 'main':
     app.run(host="localhost",port=5000, debug=True)
