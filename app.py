24782675ebb85806805cbcba98038572f33b5a6f
judyi19997 <judyi1999@kookmin.ac.kr>
2021-09-07 20:44:35 +0900

    upload s3 to .txt file

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # -- coding: utf-8 --
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 import json, boto3, os
-from werkzeug.utils import secure_filename
+from werkzeug.utils import redirect, secure_filename
 from aws_config import AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_NAME
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -24,30 +24,37 @@ def onego_recognize():
     file = ""
     filename = ""
     path = "input/manuscript_input" + filename
+    uid = request.args["uid"]
 
     if request.method == 'POST':
         file = request.file['file']
         filename = secure_filename(file.filename)
         file.save(os.path.join(path))
+
     elif request.method == 'GET':
         pred_list = ["원","고","지", " ", "인", "식", "이", " ", "돼", "어", "잇", "는", "파", "일", "입", "니", "다", "."]
-        # pred_list = start_recognize(path)
-        return jsonify({"predList":pred_list})
+        pred_str = ''.join(pred_list)
+        os.makedirs('./tmp', exist_ok=True)
+        f = open('./tmp/{}.txt'.format(uid),'w',encoding='UTF-8')
+        print('Complete create file')
+        f.write(pred_str)
+        f.close()
+        try:
+            txt_path = './tmp/{}.txt'.format(uid)
+            s3.upload_file(txt_path, AWS_S3_BUCKET_NAME, '{}/after_onego.txt'.format(uid))
+            print("complete upload .txt to s3")
+        except Exception as e:
+            print(e)
+            return "False..."
     
-@app.route("/test", methods=["GET"])
-def testPostAPI():
-    temp = request.args["uid"]
-    data = "test data"
-    return jsonify({
-        'result': data,
-        'uid' : temp,
-    })
-
 # @app.route("/test", methods=["GET"])
 # def testPostAPI():
 #     temp = request.args["uid"]
-#     data = {'txt' : "test data", 'uid' : temp}
-#     return jsonify(data)
+#     data = "test data"
+#     return jsonify({
+#         'result': data,
+#         'uid' : temp,
+#     })
 
 @app.route("/file_download", methods = ["GET"])
 def file_download():
@@ -56,7 +63,8 @@ def file_download():
     print(uid)
     try:
         s3.download_file(AWS_S3_BUCKET_NAME,"{}/Pororo.png".format(uid),file_path)
-        return "complete download"
+        print("complete download .png from s3!")
+        return redirect(url_for('onego_recognize',uid = uid))
     except Exception as e:
         print(e)
         return "flase..."
